1614b2fbed84c01a743b809a1c4b715287cd14e9
asrarali21 <mohdasrar3825@gmail.com>
2025-09-11 19:42:09 +0530

    Add CORS middleware and user model to backend

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
 import express from "express"
+import cors from "cors"
 import airouter from "./src/routes/ai.route.js"
 
 
+
 const app = express()
+
+      app.use(cors({
+            origin: 'http://localhost:5173', // Allow requests from this origin
+            methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+            allowedHeaders: ['Content-Type', 'Authorization'], // Allowed request headers
+            credentials: true // Allow sending of cookies and authorization headers
+        }));
 app.use(express.json());
 
 
